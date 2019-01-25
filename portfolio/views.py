4abc98707851f33fc7e58780432cd58053e5bc4b
vmenneni <vmenneni@paypal.com>
2019-01-25 01:16:06 -0800

    added template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+
+def home(request):
+    return render_to_response('portfolio/index.html')
